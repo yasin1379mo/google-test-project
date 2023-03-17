c5793f62a25016cee5f53d6578b1cf8242f827e7
yasin <yasin1379mo@gmail.com>
2023-03-18 02:37:24 +0330

    upload a test project of home page of google

---
+++ main.js
@@ -0,0 +1,18 @@
+function toggle(){
+        var x1 = document.getElementById("apps");
+
+        if(x1.style.display === "none") {
+                x1.style.display = "flex";
+        }else{
+                x1.style.display = "none";
+        }
+}
+function showhide(){
+        var x2 = document.getElementById("prof-menu");
+
+        if(x2.style.display === "none") {
+                x2.style.display = "inline";
+        }else{
+                x2.style.display = "none";
+        }
+}
